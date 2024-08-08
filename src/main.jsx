7d09c37e2f408c92ae4dc0c4d8bfdd752fe02bd7
mtorreblanca <matias3981@gmail.com>
2024-08-08 11:06:42 -0300

    Added Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
+		<Analytics />
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
